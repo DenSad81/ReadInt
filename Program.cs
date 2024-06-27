25b2e4730509d051eaef24867e618c812fe02574
DenSad81 <tagil81@mail.ru>
2024-06-27 15:50:32 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(GetIntFromConsole());
+        Console.WriteLine();
+    }
+
+    static int GetIntFromConsole()
+    {
+        int digitToOut = 0;
+        bool isRun = true;
+
+        while (isRun)
+        {
+            Console.Write("Введите число: ");
+            string digitFromConsole = Console.ReadLine();
+            isRun = !int.TryParse(digitFromConsole, out digitToOut);
+        }
+
+        return digitToOut;
+    }
+}
